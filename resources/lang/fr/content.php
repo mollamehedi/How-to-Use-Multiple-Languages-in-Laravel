aa9491d85a6744d600b76f4638f35977eb2fd058
molla mehedi <mollameehedi@gmail.com>
2024-07-08 21:27:29 +0600

    Readmefile Generate

--- resources/lang/fr/content.php
+++ resources/lang/fr/content.php
@@ -2,7 +2,4 @@
 
 return [
     'heading' => 'Nous verrons comment utiliser plusieurs langues dans Laravel avec Molla Mehedi',
-    'para' => 'Nous verrons comment utiliser plusieurs langues dans Laravel avec Molla Mehedi',
-    '' => 'Nous verrons comment utiliser plusieurs langues dans Laravel avec Molla Mehedi',
-    '2nd_headin'
 ];
